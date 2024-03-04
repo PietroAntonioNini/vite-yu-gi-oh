8b7b561b497efac874503d0ee6729d23edc0f625
PietroAntonioNini <151193470+PietroAntonioNini@users.noreply.github.com>
2024-03-04 16:03:17 +0100

    install sass and import API

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,23 @@
 <script>
+import axios from 'axios';
 
+export default {
+    data() {
+        return {
+            
+        }
+    },
+
+    created() {
+        axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0').then(res => {
+            console.log(res.data.data);
+        })
+    },
+
+    components: {
+
+    }
+}
 </script>
 
 <template>
