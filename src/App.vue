fd920090b420179e545dc81e245be5dcf60ba819
PietroAntonioNini <151193470+PietroAntonioNini@users.noreply.github.com>
2024-03-04 16:20:24 +0100

    Creazione "store"

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,30 @@
 <script>
 import axios from 'axios';
+import { store } from './store';
+import CardList from './components/CardList.vue';
 
 export default {
     data() {
         return {
-            
+            store,
         }
     },
 
     created() {
         axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0').then(res => {
             console.log(res.data.data);
+            this.store.cards = res.data.data
         })
     },
 
     components: {
-
+        CardList,
     }
 }
 </script>
 
 <template>
-  
+  <CardList></CardList>
 </template>
 
 <style>
