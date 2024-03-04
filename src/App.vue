9e79e18ccee982b49d7c60708f2810af79f96f36
PietroAntonioNini <151193470+PietroAntonioNini@users.noreply.github.com>
2024-03-04 17:47:34 +0100

    Fermo sul problema di visualizzazione delle img

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ export default {
     created() {
         axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0').then(res => {
             console.log(res.data.data);
-            this.store.cards = res.data.data
+            this.store.cards = res.data.data;
         })
     },
 
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -18,9 +18,14 @@ export default {
             <h2 id="nav-text">Found 39 cards</h2>
         </nav>
 
-        <div id="card-box">
-            
-        </div>
+        <ul id="cards-container">
+            <li id="cards-box" v-for="card in store.cards">
+                
+                <h3>{{ card.name }}</h3>
+                <h4>{{ card.archetype }}</h4>
+            </li>
+        </ul>
+        
     </div>
 </template>
 
@@ -36,6 +41,35 @@ export default {
 
         #nav-text {
             color: white;
+        }
+    }
+
+    #cards-container {
+        display: flex;
+        flex-flow: row wrap;
+        gap: 30px;
+
+        #cards-box {
+            width: calc(100% / 5 - 30px/5 * 4);
+
+            display: flex;
+            flex-direction: column;
+            text-align: center;
+            gap: 20px;
+
+            background-color: #D48F38;
+
+            h3 {
+                font-size: 24px;
+                font-weight: bold;
+                text-transform: uppercase;
+            }
+
+            h4 {
+                font-size: 28px;
+                font-weight: 400;
+                color: black;
+            }
         }
     }
 }
